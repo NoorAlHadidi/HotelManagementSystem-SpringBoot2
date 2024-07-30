38541e10cda00041431c2e32454a4fbca7375bfd
NoorAlHadidi <noor.ahmed.alhadidi@gmail.com>
2024-07-30 13:50:42 +0300

    mysql database configuration

--- src/main/java/com/brightskies/hotelsystem/HotelSystemApplication.java
+++ src/main/java/com/brightskies/hotelsystem/HotelSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelsystemApplication {
+public class HotelSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelsystemApplication.class, args);
+		SpringApplication.run(HotelSystemApplication.class, args);
 	}
 
 }
